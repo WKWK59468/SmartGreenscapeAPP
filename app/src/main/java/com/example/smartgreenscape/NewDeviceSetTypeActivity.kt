6f536cd9f404cbc7706b85fe09f38a223d056c25
jane0731 <jane991680420@outlook.com>
2024-01-01 15:50:13 +0800

    接植物預設數值

--- app/src/main/java/com/example/smartgreenscape/NewDeviceSetTypeActivity.kt
+++ app/src/main/java/com/example/smartgreenscape/NewDeviceSetTypeActivity.kt
@@ -2,6 +2,8 @@ package com.example.smartgreenscape
 
 import android.content.Intent
 import android.os.Bundle
+import android.content.Context
+import android.text.Editable
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
@@ -10,18 +12,20 @@ import android.widget.LinearLayout
 import android.widget.RadioButton
 import android.widget.RadioGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import com.android.volley.Request
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import com.example.smartgreenscape.databinding.ActivityNewDeviceSetTypeBinding
+import com.example.smartgreenscape.model.Plant
 import com.example.smartgreenscape.model.Tag
+import org.json.JSONArray
+import org.json.JSONObject
 
 class NewDeviceSetTypeActivity : AppCompatActivity()  {
-    private val dataList = listOf(
-        Tag(1,"tag1",20.0,30.0,50.0,70.0,50.0,70.0),
-        Tag(2,"tag2",20.0,30.0,50.0,70.0,50.0,70.0),
-        Tag(3,"tag3",20.0,30.0,50.0,70.0,50.0,70.0),
-        Tag(4,"tag4",20.0,30.0,50.0,70.0,50.0,70.0),
-    )
+    private val dataList = mutableListOf<Tag>()
     private lateinit var tag: Tag
     private lateinit var binding: ActivityNewDeviceSetTypeBinding
     private lateinit var lastPageButton: ImageButton
@@ -47,29 +51,30 @@ class NewDeviceSetTypeActivity : AppCompatActivity()  {
         cancelButton = binding.cancelButton
         deviceName = binding.deviceName
         buttonContainer = binding.buttonContainer
+        getDefaultPlantClass(){
+            for (data in dataList) {
+                val button = RadioButton(this)
+                button.text = data.name
+                button.setBackgroundResource(R.drawable.tag_button)
+                button.textSize = resources.getDimension(R.dimen.button_text_size)
 
-        for (data in dataList) {
-            val button = RadioButton(this)
-            button.text = data.name
-            button.setBackgroundResource(R.drawable.tag_button)
-            button.textSize = resources.getDimension(R.dimen.button_text_size)
+                //設定按鈕樣式
+                val params = LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.WRAP_CONTENT,
+                    resources.getDimensionPixelSize(R.dimen.fixed_height)
+                )
+                params.setMargins(0, 15, 30, 0)
+                button.layoutParams = params
+                button.setPaddingRelative(10,5,30,5)
 
-            //設定按鈕樣式
-            val params = LinearLayout.LayoutParams(
-                LinearLayout.LayoutParams.WRAP_CONTENT,
-                resources.getDimensionPixelSize(R.dimen.fixed_height)
-            )
-            params.setMargins(0, 15, 30, 0)
-            button.layoutParams = params
-            button.setPaddingRelative(10,5,30,5)
-
-            //按鈕事件監聽
-            button.setOnClickListener {
-                tag = data
+                //按鈕事件監聽
+                button.setOnClickListener {
+                    tag = data
+                }
+                buttonContainer.addView(button)
             }
-
-            buttonContainer.addView(button)
         }
+
 
         //返回按鈕
         lastPageButton.setOnClickListener{
@@ -86,6 +91,37 @@ class NewDeviceSetTypeActivity : AppCompatActivity()  {
             showAlertDialog(tag, deviceName.text.toString())
         }
     }
+    fun getDefaultPlantClass(callback: () -> Unit){
+        val queue = Volley.newRequestQueue(this)
+        val url = "http://192.168.213.10:8000/api/default-class"
+        val stringRequest = StringRequest(
+            Request.Method.GET, url,
+            { response ->
+                val jsonArray = JSONArray(response)
+                for(i in 0 until jsonArray.length()) {
+
+                    val item = jsonArray.getJSONObject(i)
+                    val plantDefaultClass = Tag(
+                        id = item.getInt("id"),
+                        name = item.getString("name") ,
+                        min_temperature = item.getDouble("min_temperature"),
+                        max_temperature= item.getDouble("max_temperature"),
+                        min_humidity= item.getDouble("min_humidity"),
+                        max_humidity= item.getDouble("max_humidity"),
+                        min_soil_humidity= item.getDouble("min_soil_humidity"),
+                        max_soil_humidity= item.getDouble("max_soil_humidity")
+                    )
+
+                    dataList.add(plantDefaultClass)
+                }
+                callback()
+            },
+            { _ ->
+                Toast.makeText(this, "取得資料失敗!", Toast.LENGTH_LONG).show();
+            })
+        queue.add(stringRequest)
+    }
+
     private fun showAlertDialog(tag: Tag?, deviceName: String) {
         val builder = AlertDialog.Builder(this)
         builder.setTitle("植物生長資訊")
@@ -117,12 +153,12 @@ class NewDeviceSetTypeActivity : AppCompatActivity()  {
             intentNewDeviceSetTypeActivity.putExtra("macAddress", macAddress)
         }
         tag?.let {
-            intentNewDeviceSetTypeActivity.putExtra("temperature_min", it.temperatureMin)
-            intentNewDeviceSetTypeActivity.putExtra("temperature_max", it.temperatureMax)
-            intentNewDeviceSetTypeActivity.putExtra("humidity_min", it.humidityMin)
-            intentNewDeviceSetTypeActivity.putExtra("humidity_max", it.humidityMax)
-            intentNewDeviceSetTypeActivity.putExtra("soil_humidity_min", it.soilHumidityMin)
-            intentNewDeviceSetTypeActivity.putExtra("soil_humidity_max", it.soilHumidityMax)
+            intentNewDeviceSetTypeActivity.putExtra("temperature_min", it.min_temperature)
+            intentNewDeviceSetTypeActivity.putExtra("temperature_max", it.max_temperature)
+            intentNewDeviceSetTypeActivity.putExtra("humidity_min", it.min_humidity)
+            intentNewDeviceSetTypeActivity.putExtra("humidity_max", it.max_humidity)
+            intentNewDeviceSetTypeActivity.putExtra("soil_humidity_min", it.min_soil_humidity)
+            intentNewDeviceSetTypeActivity.putExtra("soil_humidity_max", it.max_soil_humidity)
         }
         startActivity(intentNewDeviceSetTypeActivity)
     }
--- app/src/main/java/com/example/smartgreenscape/model/Tag.kt
+++ app/src/main/java/com/example/smartgreenscape/model/Tag.kt
@@ -3,10 +3,10 @@ package com.example.smartgreenscape.model
 data class Tag(
     val id: Int,
     var name: String,
-    var temperatureMin: Double,
-    var temperatureMax: Double,
-    var humidityMin: Double,
-    var humidityMax: Double,
-    var soilHumidityMin: Double,
-    var soilHumidityMax: Double
+    var min_temperature: Double,
+    var max_temperature: Double,
+    var min_humidity: Double,
+    var max_humidity: Double,
+    var min_soil_humidity: Double,
+    var max_soil_humidity: Double
 )
